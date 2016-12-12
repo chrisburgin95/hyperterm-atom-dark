611eea6bbb0b9c9f01367ae4b5753e5e60653c79
ChrisBurgin <chrisburgin95@gmail.com>
2016-12-12 13:58:24 -0500

    Added Dividers and Unfocused Window

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 [HyperTerm](https://hyperterm.org) theme based on [the Atom One Dark theme](https://github.com/atom/one-dark-syntax). Forked from @jxnblk's [HyperTerminator](https://github.com/jxnblk/hyperterminator) and modified by @mdo.
 
-![Screenshot at start](https://cloud.githubusercontent.com/assets/98681/16899205/f644411e-4baf-11e6-851a-930070779867.png)
-![Screenshot with tabs](https://cloud.githubusercontent.com/assets/98681/16899206/f644c080-4baf-11e6-890d-fd5c628c7991.png)
+![Screenshot at start](http://i.imgur.com/cGWN9VL.gif)
+
 
 ### Install
 
 1. Open HyperTerm's preferences with `Cmd+,` (or manually at `~/.hyperterm.js`) with your editor.
 2. Update your list of plugins to include `hyperterm-atom-dark`, like so:
-  
+
   ```js
 plugins: [
   'hyperterm-atom-dark'
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const colors = {
 exports.decorateConfig = config => {
   return Object.assign({}, config, {
     foregroundColor,
-    backgroundColor,
+    backgroundColor ,
     borderColor,
     cursorColor,
     colors,
@@ -35,7 +35,6 @@ exports.decorateConfig = config => {
       ${config.termCSS || ''}
       .cursor-node {
         mix-blend-mode: difference;
-        border-left-width: 2px;
       }
     `,
     css: `
@@ -45,114 +44,26 @@ exports.decorateConfig = config => {
         right: 0;
         left: 0;
       }
+      .splitpane_pane > .term_fit {
+        opacity: .5;
+      }
+      .term_active {
+        opacity: 1 !important;
+      }
+      .splitpane_divider {
+	      background-color: rgba(171, 178, 191, 0.15) !important;
+		  }
       .tabs_list {
         background-color: #21252b !important;
-        border-bottom-color: #181a1f !important;
-      }
-      .tab_tab {
-        font-weight: 500;
-        color: rgba(157, 165, 180, 0.6);
-        border-width: 0 0 0 1px;
-        border-image: linear-gradient(#21252b, #181a1f 1em) 0 0 0 1 stretch;
-        border-style: solid;
-      }
-      .tab_tab:first-of-type {
-        border-width: 0;
-      }
-      .tab_tab:hover {
-        color: rgba(157, 165, 180, 0.6);
-        transition: none;
-      }
-      .tab_tab::after {
-        content: "";
-        position: absolute;
-        pointer-events: none;
-        top: 0;
-        bottom: -1px;
-        left: 0;
-        width: 2px;
-        height: inherit;
-        background: #528bff;
-        opacity: 0;
-        transition: opacity .16s;
-        z-index: 1;
-      }
-      .tabs_title,
-      .tab_tab.tab_active {
-        font-weight: 500;
-        color: #d7dae0;
+        border-bottom-color: rgba(0,0,0,.15) !important;
       }
       .tab_tab.tab_active {
+        font-weight: 500;
         background-color: ${backgroundColor};
-      }
-      .tab_tab.tab_active,
-      .tab_tab.tab_active + .tab_tab {
-        border-image: linear-gradient(transparent, transparent) 0 0 0 1 stretch;
+        border-color: rgba(0,0,0,.27) !important;
       }
       .tab_tab.tab_active::before {
-        content: "";
-        z-index: 1;
-        position: absolute;
-        top: 0;
-        left: -1px;
-        bottom: -1px;
-        right: 0;
-        height: inherit;
-        background-image: linear-gradient(rgba(255, 255, 255, 0.02), rgba(255, 255, 255, 0));
-        box-shadow: inset 0 1px 1px rgba(255, 255, 255, 0.06);
-        border: 1px solid #181a1f;
         border-bottom-color: ${backgroundColor};
-        border-top: 0;
-      }
-      .tab_tab.tab_active:last-of-type::before {
-        border-right-width: 0;
-      }
-      .tab_tab.tab_active::after {
-        opacity: 1;
-        transition-duration: .32s;
-      }
-      .tab_icon {
-        display: block;
-        background: rgba(157, 165, 180, 0.6);
-        -webkit-mask-image: url('${__dirname}/close.svg');
-        mask-image: url('${__dirname}/close.svg');
-        -webkit-mask-size: 7px;
-        mask-size: 7px;
-        -webkit-mask-repeat: no-repeat;
-        mask-repeat: no-repeat;
-        -webkit-mask-position-y: center;
-        mask-position-y: center;
-        -webkit-mask-position-x: 8px;
-        mask-position-x: 8px;
-        width: 26px;
-        height: 100%;
-        top: 0;
-        right: 0;
-        transform: scale(0);
-        transition: transform .08s;
-        opacity: 1;
-        border-radius: 0;
-        z-index: 2;
-      }
-      .tab_icon:hover {
-        background: rgba(157, 165, 180, 0.6);
-        opacity: .7;
-      }
-      .tab_tab.tab_active .tab_icon {
-        background: #d7dae0;
-      }
-      .tab_tab.tab_active .tab_icon:hover {
-        background: #d7dae0;
-      }
-      .tab_tab:hover .tab_icon {
-        transform: scale(1);
-        transition-duration: .16s;
-      }
-      .tab_icon svg {
-        display: none;
-      }
-      .tab_icon::after {
-        display: none;
       }
     `
   })
